9384a0d5037e9665028f8b9f8a239b4cd93dff3c
Chris Vue <195502521+DevyAppV@users.noreply.github.com>
2025-01-27 08:36:07 -0800

    removed obj folder with .gitignore

--- Perseverance Calculator 1/obj/x64/Debug/net8.0-windows10.0.19041.0/Perseverance Calculator 1.AssemblyInfo.cs
+++ Perseverance Calculator 1/obj/x64/Debug/net8.0-windows10.0.19041.0/Perseverance Calculator 1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Perseverance Calculator 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac401cab74366838bc32cb90cc403c8e9b2a59ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b66daf40a47db4ab539924b07910f369ed9b8156")]
 [assembly: System.Reflection.AssemblyProductAttribute("Perseverance Calculator 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Perseverance Calculator 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
